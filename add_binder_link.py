2af4d76c35ce129fa12b8d5f43643068455576a0
Quentin Fortier <qpfortier@gmail.com>
2021-12-28 21:18:49 +0100

    Add force

--- add_binder_link.py
+++ add_binder_link.py
@@ -1,17 +1,9 @@
-import re
 import sys
+import re
 
-def add_link(repo : str, nb : str, env : str, branch_repo : str, branch_env : str):
-    """Add a link to binder in the title of nb
-
-    Args:
-        repo: repository of the notebook
-        nb: path to the notebook relative to repo
-        env: repository for the Binder environment
-        branch_repo: branch for repo
-        branch_env: branch for env
-    """    
+def add_link(repo, nb, env, branch_repo, branch_env, force=false):
     with open(nb, 'r') as f:
+        print(branch_env)
         lines = f.read()
         m = re.search('"# (.*)\\\\n', lines)
         if not m:
@@ -19,15 +11,17 @@ def add_link(repo : str, nb : str, env : str, branch_repo : str, branch_env : st
             return
         if "binder" in m.string:
             print(f"Binder link is already in {nb}")
-            return
+            if not force:
+                return
+            m = re.search("'>([^<])*<", m.string)
         i, j = m.start(1), m.end(1)
         title = m.group(1)
         repo_nb = repo.split("/")[-1] + "/" + nb
         def F(s): return s.replace('/', '%252F')
         url = f"https://mybinder.org/v2/gh/{ env }/{ branch_env }?urlpath=git-pull%3Frepo%3Dhttps%253A%252F%252Fgithub.com%252F{ F(repo) }%26urlpath%3Dlab%252Ftree%252F{ F(repo_nb) }%26branch%3D{ branch_repo }"
-        title = f"<center><a href='{url}'>{title} <img src=https://mybinder.org/badge.svg></a></center>"
+        title = f"<center><a href='{url}'>{title}<img src=https://mybinder.org/badge.svg></a></center>"
 
     with open(nb, 'w') as f:
         f.write(lines[:i] + title + lines[j:])
 
-add_link(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+add_link(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
